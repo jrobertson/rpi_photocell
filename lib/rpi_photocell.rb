800103305bea91f9b214bb82041d2c62e810d924
Your Name <you@example.com>
2016-03-06 22:27:05 +0000

    Now uses the mcp3008pi gem

--- lib/rpi_photocell.rb
+++ lib/rpi_photocell.rb
@@ -2,7 +2,7 @@
 
 # file: rpi_photocell.rb
 
-require 'pi_piper'
+require 'mcp3008pi'
 
 # To use this gem you will need the following:
 # 
@@ -11,81 +11,21 @@ require 'pi_piper'
 # * a Photoresistor https://en.wikipedia.org/wiki/Photoresistor
 
 
-class RPiPhotocell
+class RPiPhotocell < Mcp3008Pi
 
   def initialize(pin: 0, clock: 18, dout: 23, din: 24, cs: 25)
 
-    @adc_pin = pin
-
-    a = [clock, dout, din, cs]
-    unexport_all a
-    
-    @clock = PiPiper::Pin.new :pin => clock, :direction => :out
-    @adc_out = PiPiper::Pin.new :pin => dout
-    @adc_in = PiPiper::Pin.new :pin => din, :direction => :out
-    @cs = PiPiper::Pin.new :pin => cs, :direction => :out
-    
-    at_exit do
-      
-      # to avoid "Device or resource busy @ fptr_finalize - /sys/class/gpio/export"
-      # we unexport the pins we used
-      
-      unexport_all a
-    end      
+    super pin, clock, dout, din, cs
 
   end
 
+  # reads the analog value
+  #
   def read()
-    read_adc(@adc_pin, @clock, @adc_in, @adc_out, @cs)
+    super()
   end
 
-  private
 
-  def read_adc(adc_pin, clockpin, adc_in, adc_out, cspin)
-
-    cspin.on
-    clockpin.off
-    cspin.off
-    
-    command_out = adc_pin
-    command_out |= 0x18
-    command_out <<= 3
-
-    (0..4).each do
-      adc_in.update_value((command_out & 0x80) > 0)
-      command_out <<= 1
-      clockpin.on
-      clockpin.off
-    end
-
-    result = 0
-
-    (0..11).each do
-      clockpin.on
-      clockpin.off
-      result <<= 1
-      adc_out.read
-      if adc_out.on?
-        result |= 0x1
-      end
-    end 
-
-    cspin.on
-
-    result >> 1
-  end
-  
-  def unexport_all(pins)
-    
-    pins.each do |pin|
-      
-      next unless File.exists? '/sys/class/gpio/gpio' + pin.to_s
-
-      File.write '/sys/class/gpio/unexport', pin
-
-    end
-    
-  end  
 end
 
 
@@ -94,4 +34,3 @@ if __FILE__ == $0 then
   RPiPhotocell.new.read
 
 end
-
--- rpi_photocell.gemspec
+++ rpi_photocell.gemspec
@@ -1,10 +1,10 @@
 Gem::Specification.new do |s|
   s.name = 'rpi_photocell'
-  s.version = '0.1.0'
+  s.version = '0.2.0'
   s.summary = 'Use with a Raspberry Pi + MCP3008 10 bit analog to digital converter + photoresistor to detect ambient light intensity.'
   s.authors = ['James Robertson']
   s.files = Dir['lib/rpi_photocell.rb']
-  s.add_runtime_dependency('pi_piper', '~> 1.9', '>=1.9.9')
+  s.add_runtime_dependency('mcp3008pi', '~> 0.1', '>=0.1.0')
   s.signing_key = '../privatekeys/rpi_photocell.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
